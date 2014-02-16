b1051348f60a054b2129d52fe39e97a131dc6e7b
Maria Halacheva <mhalacheva@waverleysoftware.com>
2014-02-17 00:02:38 +0200

    dd to git ignore file, update styles, add validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,6 @@
 # Category model
 class Category < ActiveRecord::Base
-  validates :name, presence: true
-  validates :name, length: { minimum: 2 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2 }
 
   has_and_belongs_to_many :reports
 end
