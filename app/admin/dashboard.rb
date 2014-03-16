59cd34372521af688217638fb38bb1f841231d10
Maria Halacheva <mrhalacheva@gmail.com>
2014-03-16 19:42:00 +0200

    Add cancan to administration and update administration dashboard page

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,32 +2,27 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content title: "Dashboard" do
+    columns do
+      column do
+        panel "Statistics" do
+          ul do
+            li "<b>Total reports: #{Report.count}</b>".html_safe
+            li "<b>Total categories: #{Category.count}</b>".html_safe
+            li "<b>Total users: #{User.count}</b>".html_safe
+          end
+        end
+      end
+
+      column do
+        panel "Recent Reports" do
+          ul do
+            Report.last(5).each do |report|
+              li link_to(report.title, report)
+            end
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
